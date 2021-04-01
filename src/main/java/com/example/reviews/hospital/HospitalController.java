19c0aa2ddce3b7af7091bc4101776cf5bb09dbeb
sum127 <tnaud940127@naver.com>
2021-04-01 19:56:31 +0900

    0401

--- src/main/java/com/example/reviews/hospital/HospitalController.java
+++ src/main/java/com/example/reviews/hospital/HospitalController.java
@@ -3,8 +3,12 @@ package com.example.reviews.hospital;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +26,12 @@ public class HospitalController {
 
 		return list;
 
+	}
+
+	@GetMapping(value = "/hospital/paging")
+	public List<Hospital> getHospital(@RequestParam("page") int page, @RequestParam("size") int size) {
+		// �쟾泥� 紐⑸줉 議고쉶, �럹�씠吏�
+		return hospitalRepo.findAll(PageRequest.of(page, size)).toList();
 	}
 
 }
